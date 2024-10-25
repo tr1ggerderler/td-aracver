138b7fb08d377249facd4c285cc41db49f04b1ef
triggerderler <respectrigger@gmail.com>
2024-10-25 20:27:42 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -20,8 +20,8 @@ local function GenerateRandomNumbers(length)
     return randomStr
 end
 
-
-local function TakeOutVehicle(vehicleInfo, playerId)
+RegisterNetEvent('qb-vehiclejob:client:TakeOutVehicle', function(vehicleInfo)
+    local playerId = source
     local playerPed = GetPlayerPed(GetPlayerFromServerId(playerId))
     local currentVehicle = GetVehiclePedIsIn(playerPed, false)
 
@@ -29,7 +29,7 @@ local function TakeOutVehicle(vehicleInfo, playerId)
         QBCore.Functions.DeleteVehicle(currentVehicle)
     end
 
-    local coords = {x = 100.0, y = 200.0, z = 300.0, w = 0.0}
+    local coords = {x = 100.0, y = 200.0, z = 300.0}
     if coords then
         if IsAnyVehicleNearPoint(coords.x, coords.y, coords.z, 2.0) then
             QBCore.Functions.Notify("Araç garaja park edilmeli.", 'error')
@@ -37,55 +37,17 @@ local function TakeOutVehicle(vehicleInfo, playerId)
         end
 
         QBCore.Functions.SpawnVehicle(vehicleInfo.vehicle, function(veh)
-            local randomNumbers = GenerateRandomNumbers(2)  -- 2 rastgele rakam oluştur
-            local randomLetters = GenerateRandomLetters(3)  -- 3 rastgele harf oluştur
-            local randomPlate = randomNumbers .. randomLetters .. GenerateRandomNumbers(3)  -- 2 rakam + 3 harf + 3 rakam
+            local randomNumbers = GenerateRandomNumbers(2)
+            local randomLetters = GenerateRandomLetters(3)
+            local randomPlate = randomNumbers .. randomLetters .. GenerateRandomNumbers(3)
             SetVehicleNumberPlateText(veh, randomPlate)
             SetEntityHeading(veh, coords.w)
             exports['qb-fuel']:SetFuel(veh, 100.0)
             TaskWarpPedIntoVehicle(playerPed, veh, -1)
             TriggerEvent("vehiclekeys:client:SetOwner", QBCore.Functions.GetPlate(veh))
-            TriggerServerEvent("td-aracver:server:sendVehicle", vehicleInfo.vehicle, GetHashKey(vehicleInfo.vehicle), QBCore.Functions.GetPlate(veh), QBCore.Functions.GetVehicleProperties(veh))
+            TriggerServerEvent("qb-vehiclejob:server:BuyVehicle", vehicleInfo.vehicle, GetHashKey(vehicleInfo.vehicle), QBCore.Functions.GetPlate(veh), QBCore.Functions.GetVehicleProperties(veh))
             SetVehicleDirtLevel(veh, 0.0)
             SetVehicleColours(veh, 111, 111)
         end)
     end
-end
-
-
-RegisterCommand("aracver", function(source, args, rawCommand)
-    local PlayerData = QBCore.Functions.GetPlayerData()
-    local playerLicenseId = PlayerData.license
-
-    -- Yetkili Lisans ID
-    local isAllowed = false
-    for _, licenseId in ipairs(Config.AllowedLicenseIds) do
-        if playerLicenseId == licenseId then
-            isAllowed = true
-            break
-        end
-    end
-
-    if not isAllowed then
-        QBCore.Functions.Notify("Bu komutu kullanma yetkiniz yok.", "error")
-        return
-    end
-
-    if #args ~= 2 then
-        QBCore.Functions.Notify("Kullanım: /aracver [OyuncuID] [Model]", "error")
-        return
-    end
-
-    local playerId = tonumber(args[1])
-    local vehicleModel = args[2]
-
-    if not IsModelInCdimage(vehicleModel) or not IsModelAVehicle(vehicleModel) then
-        QBCore.Functions.Notify("Geçersiz araç modeli.", "error")
-        return
-    end
-
-    QBCore.Functions.Notify("Araç başarıyla verildi.", "success")
-
-    local vehicleInfo = { vehicle = vehicleModel, price = 0 }
-    TakeOutVehicle(vehicleInfo, playerId)
-end, false)
+end)
